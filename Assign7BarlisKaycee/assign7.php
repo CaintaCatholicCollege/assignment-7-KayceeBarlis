250948bbe1f6f246fa567f4300b7618512c7e3e2
Kaycee Barlis <98741287+KayceeBarlis@users.noreply.github.com>
2022-11-19 20:29:26 +0800

    Add files via upload

---
+++ Assign7BarlisKaycee/assign7.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Assignment7</title>
+</head>
+
+<body>
+
+	<?php 
+
+	$x=10; 
+	echo "Value is now " . $x . "<br>";
+ 	$x += 2;
+ 	echo "Value is now " . $x . "<br>";
+ 	$x -= 4;
+ 	echo "Value is now " . $x . "<br>";
+ 	$x *= 5;
+ 	echo "Value is now " . $x . "<br>";
+ 	$x /= 3;
+ 	echo "Value is now " . $x  . "<br>";
+ 	$x = ++$x;
+ 	echo "Value is now " . $x  . "<br>";
+ 	$x = --$x;
+ 	echo "Value is now " . $x  . "<br>";
+
+ 	?>
+	
+	
+</body>
+</html>
